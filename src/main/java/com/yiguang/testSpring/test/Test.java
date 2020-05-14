58cdc5b201be3bef28ca457eeeef93810981ddbb
yiguang <yiguang@yiguang-PC>
2020-05-14 10:50:13 +0800

    shiroServlet

---
+++ src/main/java/com/yiguang/testSpring/test/Test.java
@@ -0,0 +1,23 @@
+package com.yiguang.testSpring.test;
+
+import java.io.InputStream;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jndi.support.SimpleJndiBeanFactory;
+
+import com.yiguang.testSpring.pojo.Student;
+
+public class Test {
+	public static void main(String[] args) {
+		ApplicationContext application=new ClassPathXmlApplicationContext("spring.xml");
+		Student student=(Student) application.getBean("student1",Student.class);
+		student.setId(1);
+		student.setName("yiguang");
+		student.setSex("男");
+		System.out.println(student.getId()+"--"+student.getName());
+				
+	}
+}
